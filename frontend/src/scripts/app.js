4ad209392b20c8c0a27f65aed72b83a0e0e174ac
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2022-08-23 22:14:18 +0200

    added modal

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,4 +1,13 @@
 const cardWrapper = document.querySelector(".card-wrapper");
+const modal = document.getElementById("modal");
+const modalStart = document.getElementById("modalClick");
+const modalClose = document.querySelector(".close-btn");
+
+
+
+
+modalStart.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal);
 
 async function fetchAPI() {
   const baseURL = "https://api.punkapi.com/v2/beers?page=1&per_page=9";
@@ -14,10 +23,13 @@ function getData(results) {
   results.map(result => {
     generatedHTML += `
     <div class="col-md-4">
-    <div class="card text-center cards">
+    <div class="card text-center border-0 cards">
       <div class="card-body card-main">
-        <img class="card-img-top" src=${result.image_url} alt="image" />
+        <img class="img" src=${result.image_url} alt="image" />
         <p>$${result.abv}</p>
+        <div class="logo">
+          <i class="fa-solid fa-link fa-3x"></i>
+        </div>
       </div>
       <div class="card-footer card-bottom">
         <h5>
@@ -30,4 +42,11 @@ function getData(results) {
   `;
   });
   cardWrapper.innerHTML = generatedHTML;
+}
+
+function openModal() {
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
 }
