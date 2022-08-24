c34769239a5fd7dd5ee105fc97faffce27b7740c
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2022-08-24 19:27:58 +0200

    fix: made some changes and add styling

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,13 +1,13 @@
 const cardWrapper = document.querySelector(".card-wrapper");
 const modal = document.getElementById("modal");
-const modalStart = document.getElementById("modalClick");
-const modalClose = document.querySelector(".close-btn");
+const modalShow = document.querySelector(".modal-style");
 
-
-
-
-modalStart.addEventListener("click", openModal);
-modalClose.addEventListener("click", closeModal);
+document.addEventListener("click", function () {
+  const cards = document.querySelector(".cards");
+  cards.addEventListener("click", openModal);
+  console.log(cards);
+  document.querySelector(".close-btn").addEventListener("click", closeModal);
+});
 
 async function fetchAPI() {
   const baseURL = "https://api.punkapi.com/v2/beers?page=1&per_page=9";
@@ -15,6 +15,7 @@ async function fetchAPI() {
   const data = await response.json();
   console.log(data);
   getData(data);
+  getModal(data);
 }
 fetchAPI();
 
@@ -42,6 +43,46 @@ function getData(results) {
   `;
   });
   cardWrapper.innerHTML = generatedHTML;
+}
+
+
+function getModal(results) {
+  let generated = "";
+  results.map(result => {
+    generated += `
+ <div class="modal-content">
+        <span class="close-btn">&times;</span>
+        <div class="d-flex justify-content-between header-modal">
+          <h3>AB: ${result.ph}</h3>
+          <p>${result.name}</p>
+        </div>
+        <hr />
+        <div class="body-modal">
+          <p>With us since: <span class="text">${result.first_brewed}</span></p>
+          <p class="text">${result.description}</p>
+          <p>It goes great width:</p>
+          <p class="text">${result.food_pairing[0]}</p>
+        </div>
+        <hr />
+        <div class="footer-modal">
+          <div class="row">
+            <div class="col-lg-3">
+              <p>Abv:<br>
+              <span class="text">${result.abv}</span>
+              </p>
+            </div>
+            <div class="col-lg-9">
+              <p>
+                And Our tip:
+                <span class="text">${result.brewers_tips}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+ `;
+  });
+  modalShow.innerHTML = generated;
 }
 
 function openModal() {
