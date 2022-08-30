6662820656eaf858950c93a0189d2321e541ae97
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2022-08-31 00:41:31 +0200

    fix: working on modal

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -2,43 +2,26 @@ const cardWrapper = document.querySelector(".card-wrapper");
 const inpSearch = document.getElementById("inp-search");
 const modal = document.querySelector(".modal-style");
 const modalClose = document.querySelector(".close-modal");
-const headerTitle = document.querySelector(".header-title");
-const headerName = document.querySelector(".header-name");
-const spanText = document.querySelector(".span-text");
-const firstBodyText = document.querySelector(".first-body-text");
-const secondBodyText = document.querySelector(".second-body-text");
-const firstFooterText = document.querySelector(".first-footer-text");
-const secondFooterText = document.querySelector(".second-footer-text");
 
-modalClose.addEventListener("click", () => {
-  modal.classList = "modal-style hide";
-});
+// modalClose.addEventListener("click", () => {
+//   modal.classList.replace("show", "hide");
+// });
 
 cardWrapper.addEventListener("click", e => {
-  if (e.target.classList[0] == "fa-solid") {
-    headerTitle.innerHTML =
-      e.target.parentElement.parentElement.parentElement.parentElement.children[0].value;
-    headerName.innerHTML =
-      e.target.parentElement.parentElement.parentElement.parentElement.children[1].value;
-    spanText.innerHTML =
-      e.target.parentElement.parentElement.parentElement.parentElement.children[2].value;
-    firstBodyText.innerHTML =
-      e.target.parentElement.parentElement.parentElement.parentElement.children[3].value;
-    secondBodyText.innerHTML =
-      e.target.parentElement.parentElement.parentElement.parentElement.children[4].value;
-    firstFooterText.innerHTML =
-      e.target.parentElement.parentElement.children[1].textContent.substring(1);
-    secondFooterText.innerHTML =
-      e.target.parentElement.parentElement.parentElement.parentElement.children[5].value;
+  // if (e.target.classList[0] == "fa-solid") {
+  //   modal.classList.replace("hide", "show");
+  // }
+  const beerId = e.target.getAttribute("id");
+  let beerModal = document.getElementById(`modal-${beerId}`);
+  beerModal.classList.replace("hide", "show");
 
-    modal.classList = "modal-style show";
-  }
+  feetchBeer(beerId);
 });
 
 inpSearch.addEventListener("change", () => {
   let searchQuery = inpSearch.value.trim();
   fetchApi(searchQuery);
-  //inpSearch.value = "";
+  inpSearch.value = "";
 });
 
 async function fetchApi(query) {
@@ -60,24 +43,55 @@ async function fetchApi(query) {
   results.map(result => {
     generatedHTML += `
     <div class="col-md-4">
-    <input type="hidden" value="AB: ${result.ph}">
-    <input type="hidden" value="${result.name}">
-    <input type="hidden" value="${result.first_brewed}">
-    <input type="hidden" value="${result.description}">
-    <input type="hidden" value="${result.food_pairing}">
-    <input type="hidden" value="${result.brewers_tips}">
     <div class="card text-center border-0 cards">
-      <div class="card-body card-main">
-        <img class="img" src=${result.image_url} alt="image" />
+      <div class="card-body card-main" >
+        <img class="img" src=${result.image_url} alt="image"/>
         <p>$${result.abv}</p>
         <div class="logo">
-          <i class="fa-solid fa-link fa-3x"></i>
+          <i class="fa-solid fa-link fa-3x" id="${result.id}"></i>
         </div>
       </div>
       <div class="card-footer card-bottom">
         <h5>
-       <button class="w-100">Add to Card</button>
+          <button class="w-100">Add to Card</button>
         </h5>
+      </div>
+    </div>
+  </div>
+  <div class="modal-style hide" id="modal-${result.id}">
+    <div class="modal-content">
+      <div class="header-modal">
+        <span class="close-modal" id="${result.id}">&times;</span>
+        <div class="d-flex justify-content-between header-content">
+          <h3 class="header-title">Ab: ${result.ph}</h3>
+          <p class="header-name">${result.name}</p>
+        </div>
+      </div>
+      <hr />
+      <div class="body-modal">
+        <p>
+          With us since: <span class="span-text">${result.first_brewed}</span>
+        </p>
+        <p class="first-body-text">${result.description}</p>
+        <p>It goes great width:</p>
+        <p class="second-body-text">${result.food_pairing.join(", ")}</p>
+      </div>
+      <hr />
+      <div class="footer-modal">
+        <div class="row">
+          <div class="col-lg-3">
+            <p>
+              Abv:<br />
+              <span class="first-footer-text">${result.abv}</span>
+            </p>
+          </div>
+          <div class="col-lg-9">
+            <p>
+              And Our tip:
+              <span class="second-footer-text">${result.brewers_tips}</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -90,5 +104,11 @@ async function fetchApi(query) {
   }
   cardWrapper.innerHTML = generatedHTML;
 }
-
 fetchApi();
+
+async function feetchBeer(id) {
+  let response;
+  response = await fetch(`https://api.punkapi.com/v2/beers/${id}`);
+  let results = await response.json();
+  console.log(results);
+}
